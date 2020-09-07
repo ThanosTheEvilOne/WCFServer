c7e819da0bad1f5b1a46c7fb67b0bf7c05c0571f
Jouni Nieminen <jouninieminen85@gmail.com>
2020-09-07 12:05:11 +0300

    Pushing code

---
+++ bankaccount/bankaccount/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankaccount
+{
+    class Program
+    {
+        public class Account
+        {
+            public double balance;
+
+            public Account()
+            {
+                balance = 500.00;
+            }
+
+            public double deposit() // talletus
+            {
+                double NewBalance; // tallennus kohde
+                double deposit;
+
+                Console.WriteLine("Please entrer amount to deposit");
+                deposit = Double.Parse(Console.ReadLine());
+                NewBalance = balance + deposit;
+
+                return NewBalance;
+               
+            }
+
+            public double withdrawl()
+            {
+                double NewBalance;
+                double withdrawl;
+
+                Console.WriteLine("Please enter amount to withdrawl");
+                withdrawl = double.Parse(Console.ReadLine());
+                NewBalance = balance - withdrawl;
+
+                return NewBalance;
+            
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            double NewBalance;
+            string balance;
+            Account bank = new Account();
+            int atm = 1000, a, current, pin = 4040, pin1, pin2;
+
+            Console.WriteLine();
+            Console.WriteLine(" ////////////////////////// ");
+            Console.WriteLine(" ////////////////////////// ");
+            Console.WriteLine("______  Welcome To _________");
+            Console.WriteLine(" // Stratton Oakmon Bank // ");
+            Console.WriteLine("______ Application's _______");
+            Console.WriteLine("____________________________");
+            Console.WriteLine("____________________________");
+
+
+            Console.ReadKey();
+
+            Console.WriteLine("Input pin-code___");
+            pin1 = int.Parse(Console.ReadLine());
+
+            if(pin1==pin)
+            // Read pin
+           
+            
+
+
+            Console.WriteLine("Your current balance is 500.00");
+            Console.WriteLine("Press D for deposit or press W for withdrawl");
+
+            balance = Console.ReadLine();
+
+            if (balance == "D")
+            {
+                NewBalance = bank.deposit(); 
+                Console.WriteLine("New Balance is {0}", NewBalance);
+            }
+
+            if (balance == "W")
+            {
+                NewBalance = bank.withdrawl();
+                Console.WriteLine("New Balance is {0}", NewBalance);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ noppa/noppa/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noppa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int noppa1;
+            int noppa2;
+            int sum = 0;
+            Random rand1 = new Random(); // Satunnais kehittäjä
+            Console.WriteLine("Let's heitetään noppaa! ");
+
+
+            for (int i = 1; i < 6; i++)
+            {
+                noppa1 = rand1.Next(1, 7);
+                noppa2 = rand1.Next(1, 7);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Noppa1 heitetty summa on " + noppa1);
+                sum = sum + (noppa1 + noppa2);
+                Console.WriteLine("noppa2 heitetty summa on " + noppa2);
+
+
+
+            }
+
+            Console.WriteLine("Yhteissumma on " + sum);
+            Console.WriteLine("Pres any key to continue ");
+
+            Console.ReadKey();
+        }
+    }
+}
